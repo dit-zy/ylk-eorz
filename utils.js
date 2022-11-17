6cea8c66326534ee40b50bd14a93d7c653e28949
ditzy <ditzy.zy@pm.me>
2022-11-17 05:08:28 -0600

    wrap logic in a class

--- utils.js
+++ utils.js
@@ -1,39 +1,90 @@
-const parseDict = dictCSV => {
-  return dictCSV.split('\n')
-    .filter(line => line.trim() !== '')
-    .map(line => {
-      const cols = line.split(',')
-      return {
-        eo: cols[0],
-        dr: cols[1],
-      }
-    })
-    .reduce((acc, e) => {
-      const eo = e.eo.toLowerCase()
-      const dr = e.dr.toLowerCase()
-
-      if (!(eo in acc.eo)) {
-        acc.eo[eo] = []
-      }
-      acc.eo[eo].push(dr)
-
-      if (!(dr in acc.dr)) {
-        acc.dr[dr] = []
-      }
-      acc.dr[dr].push(eo)
-
-      return acc
-    }, { eo: {}, dr: {} })
+Set.prototype.map = function(f) {
+  return Array.from(this, f)
 }
 
-const parseText = (trDict, text) => {
+Set.prototype.join = function(sep, sorted=true) {
+  const arr = Array.from(this)
+  if (sorted) {
+    arr.sort()
+  }
+  return arr.join(sep)
+}
+
+class DravinianDict {
+
+  constructor(dictCSV) {
+
+    this.index = {}
+    this.words = {}
+
+    this.parseDict(dictCSV)
+  }
+
+  parseDict(dictCSV) {
+
+    dictCSV.split('\n')
+      .filter(line => line.trim() !== '')
+      .map(line => {
+        const cols = line.split(',')
+        return {
+          eo: cols[0],
+          dr: cols[1],
+        }
+      })
+      .forEach(e => {
+        const eo = parseWord(e.eo)
+          .reduce((acc, word) => {
+            if (!acc) {
+              if (!(word in this.words)) {
+                this.words[word] = new Set([word])
+              }
+              return word
+            } else {
+              this.words[acc].add(word)
+              return acc
+            }
+          }, '')
+
+        parseWord(e.dr)  
+          .forEach(dr => this.indexWord(dr, eo))
+      })
+  }
+
+  indexWord(dr, eo) {
+    if (!(dr in this.index)) {
+      this.index[dr] = new Set()
+    }
+    this.index[dr].add(eo)
+  }
+
+  attemptTranslate(dr) {
+    if (dr in this.index) {
+      return this.index[dr]
+        .map(eo => this.words[eo])
+        .reduce((acc, alts) => {
+          alts.forEach(eo => acc.add(eo))
+          return acc
+        }, new Set())
+    }
+    return null
+  }
+}
+
+const parseWord = (word) => {
+  return word
+    .split('/')
+    .map(word => word.trim().toLowerCase())
+}
+
+const parseText = (drDict, text) => {
   return text.split('\n')
     .map(line => line
       .split(' ')
-      .map(word => ({ dr: word }))
-      .map(word => {
-        if (word.dr in trDict.dr) {
-          word.eo = trDict.dr[word.dr]
+      .map(dr => {
+        const word = { dr: dr }
+        const eo = drDict.attemptTranslate(dr)
+        if (eo) {
+          word.eo = eo
         }
         return word
       })
@@ -44,7 +95,7 @@ const formatHtml = formattedText => {
   return formattedText
     .map(line => line
       .map(word => 'eo' in word
-        ? { class: 'eo', text: word.eo }
+        ? { class: 'eo', text: word.eo.join('/') }
         : { class: 'dr', text: word.dr })
       .map(word => `<span class="${word.class}">${word.text}</span>`)
       .join(' ')
